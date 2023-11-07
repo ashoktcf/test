f75b1d8030d07d716591351f06c5e2960fa34a81
My Name <ashoktcf@yahoo.com>
2023-11-07 18:07:56 +0530

    six seven

--- t.js
+++ t.js
@@ -324,7 +324,7 @@ const data = [
 // console.log(DescZ_A());
 // console.log(Asc0_9());
 // console.log(Desc9_0());
-console.log(DescZ_A().map((a)=>a.name).filter((a)=>a=="Patricia Lebsack"));
+// console.log(DescZ_A().map((a)=>a.name).filter((a)=>a=="Patricia Lebsack"));
 
 // console.log(data.Compare(data.name,data.name));
 
